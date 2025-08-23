47360d60e152078fa5fce9a19db34ca09e693660
allancrabelo <allanaugustocarneirorabelo@gmail.com>
2025-08-23 17:42:55 +0100

    added: initializers.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aaugusto <aaugusto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 08:57:24 by aaugusto          #+#    #+#             */
-/*   Updated: 2025/08/23 17:41:56 by aaugusto         ###   ########.fr       */
+/*   Updated: 2025/08/23 17:42:48 by aaugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ int	main(int argc, char **argv)
 	init_table(&table, argv);
 	silverware = init_silverware(&table);
 	philos = init_philos(&table, silverware);
+
 	return (0);
 }
